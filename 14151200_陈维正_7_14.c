5732af5a939d493ef72ee81c06b8627ecd99271d
2015CLanguage <buaa_2015_c@163.com>
2015-06-02 15:11:05 +0800

    Create 14151200_陈维正_7_14.c

--- 14151200_陈维正_7_14.c
+++ 14151200_陈维正_7_14.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-void f(int a[3][5])
+float f(float a[3][5])
 {
 int i,j;
 float b;
@@ -7,10 +7,12 @@ for(i=0,b=0;i<3;i++)
 	{for(j=0;j<5;j++)
 		{b=b+a[i][j];}
 	b=b/5;
-	printf("第%d个学生的平均成绩是：%lf\n",i+1,b);
+	printf("第%d个学生的平均成绩是：%4.2f\n",i+1,b);
 	}	
+return(0);
 }
-void g(int a[3][5])
+
+float g(float a[3][5])
 {
 int i,j;
 float b;
@@ -18,20 +20,19 @@ for(j=0,b=0;j<5;j++)
 	{for(i=0;i<3;i++)
 		{b=b+a[i][j];}
 	b=b/3;
-	printf("第%d科的平均成绩是：%lf\n",j+1,b);
+	printf("第%d科的平均成绩是：%4.2f\n",j+1,b);
 	}
+return(0);
 }
-
-
-
 
 void main()
 {
-int i,j,a[3][5];
+int i,j;
+float a[3][5];
 printf("请输入3个学生的成绩：\n");
 for(i=0;i<3;i++)
 	{for(j=0;j<5;j++)
-		scanf("%d",&a[i][j]);
+		scanf("%f",&a[i][j]);
 	}	
 f(a);
 g(a);
