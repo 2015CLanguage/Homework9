4a03a1d1afc02c7619e1642c3ea17576f41ec464
2015CLanguage <buaa_2015_c@163.com>
2015-06-02 18:43:23 +0800

    Update 14151193-吴偌灏-8-18.c

--- 14151193-吴偌灏-8-18.c
+++ 14151193-吴偌灏-8-18.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
 void main()
 {
-	char s[12][50]={'Janural','feberal','March','April','May','June','July','Auguest','September','Octorber','shiyiyue','December'};
+	char *s[12]={"January","Februray","March","April","May","June","July","August","September","Octorber","November","December"};
 	int i;
-	scanf("%d",i)
+	scanf("%d",&i);
+	if(i>0&&i<13)printf("%s\n",*(s+i-1));
+	else printf("input wrong\n");
 }
