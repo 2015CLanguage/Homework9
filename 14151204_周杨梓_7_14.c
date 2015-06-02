9b190afcc89ecdd26295dbbf24f274fb94f95e5b
2015CLanguage <buaa_2015_c@163.com>
2015-06-02 15:17:20 +0800

    Create 14151204_周杨梓_7_14.c

--- 14151204_周杨梓_7_14.c
+++ 14151204_周杨梓_7_14.c
@@ -1,18 +1,18 @@
 #include<stdio.h>
-void student(int a[][5],int b[],int n);
-void lesson(int a[][5],int c[],int n);
-void max(int a[][5],int n);
-void variance(int a[][5],int b[],int n);
+void student(float a[][5],float b[],int n);
+void lesson(float a[][5],float c[],int n);
+void max(float a[][5],int n);
+void variance(float a[][5],float b[],int n);
 int main()
 {
-	int a[100][5],b[100],c[5];
+	float a[100][5],b[100],c[5];
 	int n,i;
 	printf("请输入学生个数:\n");
 	scanf("%d",&n);
     printf("请输入每个学生的5门课成绩:\n");
 	for(i=0;i<n;i++)
 	{
-		scanf("%d%d%d%d%d",a[i][0],a[i][1],a[i][2],a[i][3],a[i][4]);
+		scanf("%f%f%f%f%f",&a[i][0],&a[i][1],&a[i][2],&a[i][3],&a[i][4]);
 	}
     student(a,b,n);
 	lesson(a,c,n);
@@ -20,9 +20,10 @@ int main()
 	variance(a,b,n);
 	return 0;
 }
-void student(int a[][5],int b[],int n)
+void student(float a[][5],float b[],int n)
 {
-	int i,j,s;
+	int i,j;
+	float s;
 	for(i=0;i<n;i++)
 	{
 		for(j=0,s=0;j<5;j++)
@@ -33,12 +34,13 @@ void student(int a[][5],int b[],int n)
 	}
 	for(i=0;i<n;i++)
 	{
-	    printf("第%d个学生的平均分为:%d\n",i+1,b[i]);
+	    printf("第%d个学生的平均分为:%f\n",i+1,b[i]);
 	}
 }
-void lesson(int a[][5],int c[],int n)
+void lesson(float a[][5],float c[],int n)
 {
-	int i,j,s;
+	int i,j;
+	float s;
 	for(i=0;i<5;i++)
 	{
 		for(j=0,s=0;j<n;j++)
@@ -49,12 +51,13 @@ void lesson(int a[][5],int c[],int n)
 	}
 	for(i=0;i<n;i++)
 	{
-	    printf("第%d门课的平均分为:%d\n",i+1,c[i]);
+	    printf("第%d门课的平均分为:%f\n",i+1,c[i]);
 	}
 }
-void max(int a[][5],int n)
+void max(float a[][5],int n)
 {
-	int i,j,max,s,l;
+	int i,j,s,l;
+	float max;
 	max=a[0][0];
 	for(i=0;i<n;i++)
 	{
@@ -68,11 +71,12 @@ void max(int a[][5],int n)
 			}
 		}
 	}
-	printf("所有50个分数中最高的分数为:%d,对应的学生是第%d个，对应课程是第%d门课\n",max,s,l);
+	printf("所有50个分数中最高的分数为:%f,对应的学生是第%d个，对应课程是第%d门课\n",max,s,l);
 }
-void variance(int a[][5],int b[],int n)
+void variance(float a[][5],float b[],int n)
 {
-	int d,i,s,s2;
+	int i;
+	float s,s2,d;
 	s=0;
 	s2=0;
 	for(i=0;i<n;i++)
@@ -84,5 +88,5 @@ void variance(int a[][5],int b[],int n)
 		s2=s2+b[i]*b[i];
 	}
 	d=s2/n-(s/n)*(s/n);
-	printf("平均分方差为:%d\n",d);
+	printf("平均分方差为:%f\n",d);
 }
