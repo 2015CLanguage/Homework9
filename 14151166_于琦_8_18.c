610d45dfad281f6fe496ce75f390ef3009442751
2015CLanguage <buaa_2015_c@163.com>
2015-06-02 19:11:40 +0800

    Create 14151166_于琦_8_18.c

---
+++ 14151166_于琦_8_18.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main()
+{
+    char *p[12]={"January","February","March","April","May","June","July","August","September","October","November","December"};
+    int n;
+    printf("请输入月份n");
+    scanf("%d",&n);
+    if((n>=1)&&(n<=12))
+        printf(*(p+n-1));
+    else
+        printf("输入错误");
+}
