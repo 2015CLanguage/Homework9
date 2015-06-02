d98585e083475922ffb8820877a2daaa1640def7
2015CLanguage <buaa_2015_c@163.com>
2015-06-03 00:55:12 +0800

    Create 14151140_张天佑_8.17.c

---
+++ 14151140_张天佑_8.17.c
@@ -0,0 +1,24 @@
+#include "stdio.h"
+int strcmp(char *p1, char *p2)
+{
+	int i = 0;
+	while (*(p1 + i) == *(p2 + i))
+	{
+		if (*(p1 + i) == '\0')
+			return 0;
+		i++;
+	}
+	return(*(p1 + i) - *(p2 + i));
+}
+void main()
+{
+	int a;
+	char str1[20], str2[20], *p1, *p2;
+	printf("输入字符串:\n");
+	scanf("%s", str1);
+	scanf("%s", str2);
+	p1 = str1;
+	p2 = str2;
+	a = strcmp(p1, p2);
+	printf("结果为:%d\n", a);
+}
