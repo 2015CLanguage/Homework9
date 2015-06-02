14a5a8d706d71cdb56e3b9d7da5ea9f267c8ba13
2015CLanguage <buaa_2015_c@163.com>
2015-06-02 15:15:26 +0800

    Create 14151193-吴偌灏-8-18.c

---
+++ 14151193-吴偌灏-8-18.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+void main()
+{
+	char s[12][50]={'Janural','feberal','March','April','May','June','July','Auguest','September','Octorber','shiyiyue','December'};
+	int i;
+	scanf("%d",i)
+}
