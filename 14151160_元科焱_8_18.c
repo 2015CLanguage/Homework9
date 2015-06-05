9927e1ce3796f53908161aa6c69b91841dff5e90
2015CLanguage <buaa_2015_c@163.com>
2015-06-05 13:49:03 +0800

    Create 14151160_元科焱_8_18.c

---
+++ 14151160_元科焱_8_18.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+char *st[13]={"wrong","Junary","February","March","April","May","June","July","August","Sepetember","Ocotober","November","December"};
+int n;
+int main()
+{
+    scanf("%d",&n);
+    if (n>12||n<1) 
+      printf("%s\n",*st);
+      else printf("%s",*(n+st));
+      return 0;
+}
