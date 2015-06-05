466d6098aa73f4891bf3cd855ce923cc32d08ab2
2015CLanguage <buaa_2015_c@163.com>
2015-06-05 13:45:40 +0800

    Create 14151160_元科焱_7_18.c

---
+++ 14151160_元科焱_7_18.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+void day(int a,int b,int c)
+{
+	int i,s=0;
+	for(i=1;i<b;i++)
+	{
+		switch(i)
+		{
+		    case 1: s+=31;break;
+			case 3: s+=31;break;
+			case 4: s+=30;break;
+			case 5: s+=31;break;
+			case 6: s+=30;break;
+			case 7: s+=31;break;
+			case 8: s+=31;break;
+			case 9: s+=30;break;
+			case 10: s+=31;break;
+			case 11: s+=30;break;
+			case 12: s+=31;break;
+			default:{
+				       if(a%4==0 && a%100!=0 || a%400==0) s+=29;
+				       else s+=28;
+					}
+		}
+	}
+	s+=c;
+	printf("第%d天\n",s);
+}
+
+void main()
+{
+	int a,b,c;
+    puts("输入年:");
+	scanf("%d",&a);
+    puts("输入月:");
+	scanf("%d",&b);
+	puts("输入日:");
+	scanf("%d",&c);
+	printf("这天是%d年:",a);
+	day(a,b,c);
+}
