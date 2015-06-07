2dafd86bd88aad979fe3af8fbd6170a38190a1fc
2015CLanguage <buaa_2015_c@163.com>
2015-06-07 23:21:51 +0800

    Update 14151200_陈维正_7_14.c

--- 14151200_陈维正_7_14.c
+++ 14151200_陈维正_7_14.c
@@ -1,39 +1,78 @@
-#include <stdio.h>
-float f(float a[3][5])
+#include<stdio.h>
+void f(float a[10][5]);
+void g(float a[10][5]);
+void h(float a[10][5]);
+void p(float a[10][5]);
+int main()
 {
-int i,j;
-float b;
-for(i=0,b=0;i<3;i++)
-	{for(j=0;j<5;j++)
-		{b=b+a[i][j];}
-	b=b/5;
-	printf("第%d个学生的平均成绩是：%4.2f\n",i+1,b);
-	}	
-return(0);
+	int i,j;
+	float a[10][5];
+	printf("请依次输入10名学生的5门课程的分数");
+	for(i=0;i<10;i++)
+		for(j=0;j<5;j++)
+			scanf("%f",&a[i][j]);
+	f(a);
+	g(a);
+	h(a);
+ 	p(a);
+	return 0;
 }
-
-float g(float a[3][5])
+void f(float a[10][5])
 {
-int i,j;
-float b;
-for(j=0,b=0;j<5;j++)
-	{for(i=0;i<3;i++)
-		{b=b+a[i][j];}
-	b=b/3;
-	printf("第%d科的平均成绩是：%4.2f\n",j+1,b);
-	}
-return(0);
+	float b[10],c[10];
+	int i,j;
+	for(i=0;i<10;i++)
+		b[i]=0;
+    for(i=0;i<10;i++)
+		for(j=0;j<5;j++)
+			b[i]=b[i]+a[i][j];
+	for(i=0;i<10;i++)
+		c[i]=b[i]/5;
+    printf("这十名学生的平均分分别为");
+	for(i=0;i<10;i++)
+		printf("%4f  ",c[i]);
+	printf("\n");
 }
-
-void main()
+void g(float a[10][5])
 {
-int i,j;
-float a[3][5];
-printf("请输入3个学生的成绩：\n");
-for(i=0;i<3;i++)
-	{for(j=0;j<5;j++)
-		scanf("%f",&a[i][j]);
-	}	
-f(a);
-g(a);
+	float d[5],e[5];
+	int i,j;
+	for(j=0;j<5;j++)
+		d[j]=0;
+    for(j=0;j<5;j++)
+		for(i=0;i<10;i++)
+            d[j]=d[j]+a[i][j];
+    for(j=0;j<5;j++)
+		e[j]=d[j]/10;
+	printf("这五门课的平均成绩分别为");
+	for(j=0;j<5;j++)
+		printf("%4f  ",e[j]);
+	printf("\n");
+}
+void h(float a[10][5])
+{
+	int i,j,x,y;
+	float max;
+	max=a[0][0];
+    for(i=0;i<10;i++)
+		for(j=0;j<5;j++)
+			if(max<a[i][j])
+                max=a[i][j],x=i,y=j;
+	printf("最高的成绩是第%d位同学的第%d科,为%5f分\n",i,j,max);
+}
+void p(float a[10][5])
+{
+    float b[10],c[10],q,s,t;
+	int i,j;
+	t=0,s=0;
+	for(i=0;i<10;i++)
+		b[i]=0;
+    for(i=0;i<10;i++)
+		for(j=0;j<5;j++)
+			b[i]=b[i]+a[i][j];
+	for(i=0;i<10;i++)
+		c[i]=b[i]/5,t=t+c[i],s=s+c[i]*c[i];
+    q=s/5-(t/5)*(t/5);
+	printf("平均分方差为%4f  \n",q);
+	printf("\n");
 }
