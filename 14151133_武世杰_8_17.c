9e0229e59299f9db65a555325a1002463f82244e
2015CLanguage <buaa_2015_c@163.com>
2015-06-07 22:35:45 +0800

    Create 14151133_武世杰_8_17.c

---
+++ 14151133_武世杰_8_17.c
@@ -0,0 +1,26 @@
+#include "stdio.h"
+int strcmp(char *p1, char *p2)
+{
+	int i = 0;
+	while (*(p1 + i) == *(p2 + i))
+	{
+		if (*(p1 + i) == '\0')
+			return 0;
+		i++;
+	}
+	return(*(p1 + i) - *(p2 + i));
+}
+int main()
+{
+	int m;
+	char s1[50], s2[50], *p1, *p2;
+	printf("请输入两个字符串\n");
+	scanf_s("%s", s1);
+	scanf_s("%s", s2);
+	p1 = &s1[0];
+	p2 = &s2[0];
+	m = strcmp(p1, p2);
+	printf("结果是:%d", m);
+	printf("\n");
+	return 0;
+}
